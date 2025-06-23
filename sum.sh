33461302009ef583fc23e237dd2a0c5f7ae8586a
Yongjun OH <dhdydwns988@gmail.com>
2025-06-23 15:00:06 +0900

    change func

--- sum.sh
+++ sum.sh
@@ -5,7 +5,6 @@ source "$(dirname "$0")/.env"
 DATE=$(date +"%Y-%m-%d")
 
 CPU_MEM_FILE="$LOG_DIR/cpu_mem_$DATE.log"
-DISK_FILE="$LOG_DIR/disk_detail_$DATE.log"
 OUT_FILE="$LOG_DIR/summary_$DATE.log"
 
 # === CPU 평균 계산 ===
@@ -13,34 +12,16 @@ CPU_AVG=$(awk -F'CPU:' '{split($2,a,","); sum+=a[1]; n++} END {if(n>0) printf "%
 MEM_AVG=$(awk -F'MEM:' '{sum+=$2; n++} END {if(n>0) printf "%.2f", sum/n; else print "N/A"}' "$CPU_MEM_FILE")
 
 # === 디스크 평균 계산 함수 ===
-disk_avg_info() {
-    grep "$1" "$DISK_FILE" | awk -F',' -v target="$1" '
-    {
-        gsub("G", "", $3); # Used
-        gsub("G", "", $4); # Size
-        gsub("%", "", $6); # Use%
-        total_size+=$3;
-        total_used+=$4;
-        total_percent+=$6;
-        count++;
-    }
-    END {
-        if (count > 0) {
-            avg_percent = total_percent / count;
-            avg_used = total_used / count;
-            avg_size = total_size / count;
-            printf "%.2f|%.0fG|%.0fG", avg_percent, avg_used, avg_size;
-        } else {
-            print "N/A|N/A|N/A";
-        }
-    }'
-}
+disk_usage() {
+    mount_point=$1
 
-# '/' 평균
-ROOT_INFO=$(disk_avg_info "/")
-ROOT_PERCENT=$(echo $ROOT_INFO | cut -d'|' -f1)
-ROOT_USED=$(echo $ROOT_INFO | cut -d'|' -f2)
-ROOT_SIZE=$(echo $ROOT_INFO | cut -d'|' -f3)
+    if [ -z "$mount_point" ]; then
+      echo "Usage: $0 <mount_point>"
+      exit 1
+    fi
+
+    df -h "$mount_point" | awk 'NR==1 {next} {printf "%s usage: %s (total: %s)\n", $6, $3, $2}'
+}
 
 # uptime
 UPTIME=$(uptime -p)
@@ -51,6 +32,6 @@ echo "===== $DATE daily usage average =====" > "$OUT_FILE"
 echo "CPU usage(%): $CPU_AVG" >> "$OUT_FILE"
 echo "MEM usage(%): $MEM_AVG" >> "$OUT_FILE"
 
-echo "/ usage: $ROOT_USED (total: $ROOT_SIZE)" >> "$OUT_FILE"
+echo $(disk_usage "/")  >> "$OUT_FILE"
 
 echo "Uptime: $UPTIME" >> "$OUT_FILE"
